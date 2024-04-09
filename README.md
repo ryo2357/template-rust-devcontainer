a4dc82093b204cf2d37272e12a81e184ead5a1c3
ryo2357 <ryosuke.m.ab@gmail.com>
2024-04-09 15:57:52 +0900

    初期設定

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# template-rust-devcontainer
+
+Rust development template on devcontainer
+
+## Setup
+
+Change project name to repository name.
+
+`bash setup.bash`
+
+## Make Docker Image
+
+- build: `docker build -t <image_name> .`
+- run: `docker run -it <image_name>`
+- run and delete imatge: `docker run -it --rm <image_name>`
+
--- setup.bash
+++ setup.bash
@@ -1,19 +1,12 @@
 #!/bin/bash
 
-# 現在のディレクトリ内のすべてのディレクトリをループ処理
-# for dir in */; do
-#     # ディレクトリ名を取得（末尾の/を削除）
-#     dirname=${dir%*/}
-    
-#     # ファイルが存在する場合のみ処理を実行
-#     if [[ -f "${dir}file.txt" ]]; then
-#         # ファイル内の文字列'DIRECTORY_NAME'をディレクトリ名で置換
-#         sed -i "s/DIRECTORY_NAME/${dirname}/g" "${dir}file.txt"
-#     fi
-
-# dirname=${dir%*/}
-
 current_dir=$(pwd)
 project_name=$(basename $current_dir)
 
-echo $parent_dir_name
+sed -i "s/template-rust-devcontainer/$project_name/g" Cargo.toml
+sed -i "s/template-rust-devcontainer/$project_name/g" Dockerfile
+sed -i "s/template-rust-devcontainer/test/g" .devcontainer/devcontainer.json
+
+
+echo "Setup complete"
+rm -- "$0"
